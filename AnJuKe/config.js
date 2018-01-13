67eaed4c6a102f2ee8fa4825d1d327391127694d
pxj <13797523276@163.com>
2018-01-13 16:02:50 +0800

    Add config files

---
+++ AnJuKe/config.js
@@ -0,0 +1,15 @@
+module.exports={
+  //端口
+  port: 8888,
+
+  //数据库
+  mysql_host: 'localhost',
+  mysql_port: 3306,
+  mysql_user: 'root',
+  mysql_password: 'pxj1989',
+  mysql_dbname: 'anjuke',
+
+  //超级管理员配置
+  root_username: 'pxj',
+  root_password: '202cb962ac59075b964b07152d234b70',
+};
